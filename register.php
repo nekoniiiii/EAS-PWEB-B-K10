82bdbd0f7b8714322e0f84c7422d4e0b60020b10
Neisa <72701806+nekoniiiii@users.noreply.github.com>
2022-12-16 21:06:41 +0700

    change name

--- register.php
+++ register.php
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
  
     <link rel="stylesheet" type="text/css" href="style.css">
  
-    <title>Niagahoster Register</title>
+    <title>Register</title>
 </head>
 <body>
     <div class="container">
